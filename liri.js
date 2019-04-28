186658428634691645622c13f193227bd4950464
Kevin Stokes <kevinstokes@kevins-mbp.attlocal.net>
2019-04-27 22:42:37 -0400

    spotify call works if song defined

--- liri.js
+++ liri.js
@@ -8,17 +8,54 @@ var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
-//var runThis = function(argOne, argTwo) {
-  //  pick(argOne, argTwo);
-//};
+//getMeSpotify function
 
-//runThis(process.argv[2], process.argv[3]);
+var getMeSpotify = function(songName) {
  
- 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
+    spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+
+    console.log("-----Artist:-----");
+    console.log(data.tracks.items[0].artists[0].name);
+    console.log("-----Song Name:-----");
+    console.log(data.tracks.items[0].name);
+    console.log("-----Preview link of song:-----");
+    console.log(data.tracks.items[0].preview_url);
+    console.log("-----Album the song is from:-----");
+    console.log(data.tracks.items[0].album.name);
+    })
+    
+    }
+
+    // end of getMeSpotify function
+
+// switch statement to use one of the 3 APIs
+
+var pick = function(caseData, functionData) {
+    switch(caseData) {
+        case 'spotify-this-song':
+        getMeSpotify(functionData);
+        break;
+        default:
+        console.log("please enter a song");
+    }
+}
+
+// end of switch statement
+
+// defining function for what user inputs
+
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+
+// end of defining function for what user inputs
+
+// calling runThis function
+
+runThis(process.argv[2], process.argv[3]);
+
+// end of runThis function
+
