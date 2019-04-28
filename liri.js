470820dd59c6be744d2b7df0fc6d8fb023df614e
Kevin Stokes <kevinstokes@kevins-mbp.attlocal.net>
2019-04-27 20:46:04 -0400

    pulling spotify objects

--- liri.js
+++ liri.js
@@ -1,5 +1,24 @@
+// REQUIRE .env FILE
 require("dotenv").config();
 
+// Require keys
 var keys = require("./keys.js");
 
+// Require Spotify
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+
+//var runThis = function(argOne, argTwo) {
+  //  pick(argOne, argTwo);
+//};
+
+//runThis(process.argv[2], process.argv[3]);
+ 
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
